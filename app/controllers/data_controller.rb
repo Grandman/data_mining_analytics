8bc4fb2961e78d752aec42e86ee8df1775a4ab05
Grandman <root@grandman73.ru>
2015-01-23 00:37:17 +0300

    добавлено резюмирование

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -3,18 +3,7 @@ class DataController < ApplicationController
 
   end
   def data
-    ga = Gattica.new({
-    :email => params[:email],
-    :password => params[:password]
-                 })
-    ga.profile_id = params[:profile_id]
-    data = ga.get({
-    :start_date   => (Date.today-1.month).to_s,
-    :end_date     => Date.today.to_s,
-    :dimensions   => ['source'],
-    :metrics      => [ 'bounceRate', 'avgsessionDuration', 'visits'],
-
-            })
+    data = google_get ['source'], [ 'bounceRate', 'avgsessionDuration', 'visits']
     data_json = data.to_h['points'].to_json
     parsed_json = ActiveSupport::JSON.decode(data_json)
     data_table = GoogleVisualr::DataTable.new
@@ -23,6 +12,7 @@ class DataController < ApplicationController
     data_table.new_column('number', 'Кластер 2')
     data_table.new_column('number', 'Кластер 3')
     data_table.new_column('number', 'Кластер 4')
+    all_sources = parsed_json.map {|item| item['source']}
     data_prepared = parsed_json.map { |item| [item['bounceRate'].to_f, item['avgsessionDuration'].to_f] }
     kmeans = KMeans.new(data_prepared, :centrods => 4, :distance_measure => :euclidean_distance)
     result = kmeans.view
@@ -40,25 +30,21 @@ class DataController < ApplicationController
     
     }
     @chart = GoogleVisualr::Interactive::ScatterChart.new(data_table, opts)
-    data1 = ga.get({
-    :start_date   => (Date.today-1.month).to_s,
-    :end_date     => Date.today.to_s,
-    :dimensions   => ['source','date'],
-    :metrics      => [ 'bounceRate', 'avgsessionDuration', 'visits'],
-
-            })
+    data1 = google_get  ['source','date'], [ 'bounceRate', 'avgsessionDuration', 'visits']
     s = []
     data_json = data1.to_h['points'].to_json
     parsed_json1 = ActiveSupport::JSON.decode(data_json)
     parsed_json1.sort_by{ |item| item['date'] }
     visits = []
-    visits[0] = {}
-    visits[1] = {}
-    visits[2] = {}
-    visits[3] = {}
+    sources = []
+    (0..3).each do |index|
+      visits[index] = {}
+      sources[index] = []
+    end
     result.each_with_index do |cluster, index|
       cluster.each do |item|
         array = parsed_json1.select { |row| row['source'] == parsed_json[item]['source'] }  
+        sources[index] << all_sources[item]  
         array.each { |arr| if visits[index][arr['date']].nil? then visits[index][arr['date']] = 0 else visits[index][arr['date']] += arr['visits'].to_i end }   
       end
     end
@@ -74,6 +60,7 @@ class DataController < ApplicationController
         max = item.length 
         max_index = index
      end
+    @sources = sources[max_index]
     end
     opts   = {
       :width => 800, :height => 800, :title => "Прогнозирование для кластера #{max_index + 1}",
@@ -92,8 +79,16 @@ class DataController < ApplicationController
       s[index+1] = 0.8*item[1]+(1-0.8)*s[index-1]
       data_table1.add_row([Date.parse(item[0]), nil, s[index]])
     end 
+    count_days = s.length
+    params[:months].to_i.times do |num|
+      s[count_days+num] =  0.8*s[count_days+num-2]+(1-0.8)*s[count_days+num-2]
+      data_table1.add_row([Date.today+num.day, nil, s[count_days+num]])
+    end
     @chart1 = GoogleVisualr::Interactive::LineChart.new(data_table1, opts)
     @data = array_of_max_cluster 
+    visits_length = array_of_max_cluster.length-1
+    @increase = array_of_max_cluster[visits_length][1]<s[visits_length]
+    @diff = "За вчера: #{array_of_max_cluster[visits_length][1]}; прогнозируется за сегодня: #{s[visits_length].to_i}" 
   end
   def select_account
     if params[:password].nil? || params[:email].nil?
@@ -107,4 +102,21 @@ class DataController < ApplicationController
     @email = params[:email]
     @password = params[:password]
   end
+  protected 
+  
+  def google_get dimensions, metrics
+    ga = Gattica.new({
+    :email => params[:email],
+    :password => params[:password]
+                 })
+    ga.profile_id = params[:profile_id]
+    ga.get({
+        :start_date   => (Date.today-params[:months].to_i.month).to_s,
+        :end_date     => Date.today.to_s,
+        :dimensions   => dimensions,
+        :metrics      => metrics,
+
+            })
+  end
+ 
 end
